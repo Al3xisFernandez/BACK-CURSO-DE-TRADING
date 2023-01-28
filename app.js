d01bcd524f82ee2698cde4c359d5ce6d7829226e
Al3xisFernandez <alexxxfernandez50@gmail.com>
2023-01-28 15:17:57 -0300

    cambiosBack

--- app.js
+++ app.js
@@ -17,17 +17,18 @@ mongoose
     console.log("connected to database");
   })
   .catch((e) => console.log(e));
-require("./userDetail");
+require("./UserDetail");
 
 const User = mongoose.model("UserInfo");
 //api de registro
 app.post("/register", async (req, res) => {
   const { fname, lname, email, password } = req.body;
+
   const encrytedPassword = await bcrypt.hash(password, 10);
   try {
     const oldUser = await User.findOne({ email });
     if (oldUser) {
-      res.json({ error: "User Exists" });
+      return res.json({ error: "User Exists" });
     }
     await User.create({
       fname,
@@ -46,16 +47,16 @@ app.post("/loginuser", async (req, res) => {
   //verificando si el usuario existe o no
   const user = await User.findOne({ email });
   if(!user){
-    return res.json({ error: "User Not Found" });
+     res.json({ error: "User Not Found" });
   }
   //verificando la contraseña comparando
-  if( await bcrypt.compare(password, user.password)){
+  if( await bcrypt.compare(password, user.password)){ 
     const token = jwt.sign({email: user.email}, JWT_SECRET, {
       expiresIn: "1h",
     }) ;
     //aqui  muesta el status de como sallo el login
     if(res.status(201)){
-      return res.json({ status : "ok", data:token});
+      return res.json({ status : "ok", data: token });
     }else{
       return res.json({ error : "error"});
     }
@@ -65,7 +66,6 @@ app.post("/loginuser", async (req, res) => {
 //api data
 app.post("/userdata", async (req, res) =>{
   const {token} = req.body;
-  let error = null;
   try{
     const user = jwt.verify(token, JWT_SECRET, (err, res)=>{
       if(err){
@@ -78,7 +78,7 @@ app.post("/userdata", async (req, res) =>{
       return res.json({status})
     }
     const useremail = user.email;
-    User.findOne({email : useremail}).then((data) =>{
+    User.findOne({ email : useremail}).then((data) =>{
       res.send({ status: "ok", data: data});
      })
      .catch((error) => {
